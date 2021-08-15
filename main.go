4eed9e30f278ad2a892864b031444a5333399a48
jh <neosouler@gmail.com>
2021-08-15 11:45:46 +0900

    add flag on cli

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"log"
 	"neosouler7/bookstore-go/binance"
@@ -14,36 +15,43 @@ import (
 )
 
 var (
-	errExecFormat       = errors.New("[ERROR] ex) go run main.go {exchange}")
 	errExchangeNotFound = errors.New("[ERROR] enter proper exchange")
 )
 
+func usage() {
+	fmt.Print("Welcome to bookstore-go\n\n")
+	fmt.Print("Please use the following commands\n\n")
+	fmt.Print("-e : Set exchange code to run\n")
+	os.Exit(0)
+}
+
 func main() {
 	args := os.Args
-	if len(args) != 2 {
-		log.Fatalln(errExecFormat)
+	if len(args) == 1 {
+		usage()
 	}
 
-	exchange := args[1]
-	tgMsg := fmt.Sprintf("[bookstore-go] %s\n", exchange)
+	exchange := flag.String("e", "", "Set exchange code to run")
+	flag.Parse()
 
-	switch exchange {
+	tgMsg := fmt.Sprintf("[bookstore-go] %s\n", *exchange)
+	switch *exchange {
 	default:
 		log.Fatalln(errExchangeNotFound)
 	case "upb":
 		tgmanager.SendMsg(tgMsg)
-		upbit.Run(exchange)
+		upbit.Run(*exchange)
 	case "con":
 		tgmanager.SendMsg(tgMsg)
-		coinone.Run(exchange)
+		coinone.Run(*exchange)
 	case "bin":
 		tgmanager.SendMsg(tgMsg)
-		binance.Run(exchange)
+		binance.Run(*exchange)
 	case "kbt":
 		tgmanager.SendMsg(tgMsg)
-		korbit.Run(exchange)
+		korbit.Run(*exchange)
 	case "hbk":
 		tgmanager.SendMsg(tgMsg)
-		huobikorea.Run(exchange)
+		huobikorea.Run(*exchange)
 	}
 }
