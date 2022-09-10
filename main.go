3fe864a902016ac03003c73ca6f7b80cb2c0361a
jh <neosouler@gmail.com>
2022-09-10 18:00:04 +0900

    add GC tracker

--- main.go
+++ main.go
@@ -33,6 +33,19 @@ func main() {
 	// 	log.Println(http.ListenAndServe("localhost:6060", nil))
 	// }()
 
+	// for profiling
+	// go func() {
+	// 	for {
+	// 		var m runtime.MemStats
+	// 		runtime.ReadMemStats(&m)
+	// 		fmt.Printf("HeapAlloc = %v", (m.HeapAlloc))
+	// 		fmt.Printf("\tHeapObjects = %v", (m.HeapObjects))
+	// 		fmt.Printf("\tHeapSys = %v", (m.Sys))
+	// 		fmt.Printf("\tNumGC = %v\n", m.NumGC)
+	// 		time.Sleep(5 * time.Second)
+	// 	}
+	// }()
+
 	args := os.Args
 	if len(args) == 1 {
 		usage()
