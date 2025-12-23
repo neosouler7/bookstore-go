7af424afd334475a42d90ab3209ef859fb8e1f7a
JH <neosouler@gmail.com>
2025-12-23 19:47:10 +0900

    bug fix

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func main() {
 		commons.SetTimeZone("Tg"),
 	)
 
-	tgMsg := fmt.Sprintf("## START %s %s\n- version 1.1.29", config.GetName(), *exchange)
+	tgMsg := fmt.Sprintf("## START %s %s\n- version 1.1.30", config.GetName(), *exchange)
 	switch *exchange {
 	default:
 		usage()
